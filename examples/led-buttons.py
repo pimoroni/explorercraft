53b5792813f9b91d18bc8d2aa5f688c5313e4c2e
Philip Howard <phil@gadgetoid.com>
2015-06-03 14:27:04 +0100

    Added on/off indicator

--- examples/led-buttons.py
+++ examples/led-buttons.py
@@ -2,23 +2,38 @@ import explorercraft
 import explorerhat
 import signal
 
-mc = explorercraft.MinecraftInstanceHandler.Instance()
+mc  = explorercraft.MinecraftInstanceHandler.Instance()
+
+LED_OFF = explorercraft.OBSIDIAN
+LED_ON  = explorercraft.GLOWING_OBSIDIAN
 
 def red_led(x, y, z, type, face):
-    if face == explorercraft.BLOCK_TOP:
-        explorerhat.light.red.toggle()
+    explorerhat.light.red.toggle()
+    if explorerhat.light.red.read():
+    	mc.setBlock(x,y+1,z,LED_ON)
+    else:
+    	mc.setBlock(x,y+1,z,LED_OFF)
 
 def blue_led(x, y, z, type, face):
-    if face == explorercraft.BLOCK_TOP:
-        explorerhat.light.blue.toggle()
+    explorerhat.light.blue.toggle()
+    if explorerhat.light.blue.read():
+    	mc.setBlock(x,y+1,z,LED_ON)
+    else:
+    	mc.setBlock(x,y+1,z,LED_OFF)
 
 def yellow_led(x, y, z, type, face):
-    if face == explorercraft.BLOCK_TOP:
-        explorerhat.light.yellow.toggle()
+    explorerhat.light.yellow.toggle()
+    if explorerhat.light.yellow.read():
+    	mc.setBlock(x,y+1,z,LED_ON)
+    else:
+    	mc.setBlock(x,y+1,z,LED_OFF)
 
 def green_led(x, y, z, type, face):
-    if face == explorercraft.BLOCK_TOP:
-        explorerhat.light.green.toggle()
+    explorerhat.light.green.toggle()
+    if explorerhat.light.green.read():
+    	mc.setBlock(x,y+1,z,LED_ON)
+    else:
+    	mc.setBlock(x,y+1,z,LED_OFF)
 
 mc.on_hit(handler=red_led,    block_type=explorercraft.WOOL_RED)
 mc.on_hit(handler=blue_led,   block_type=explorercraft.WOOL_BLUE)
